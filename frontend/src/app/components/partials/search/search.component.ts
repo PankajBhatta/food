c8da7418a3e05393dff2659db635df1839d2b8ea
PankajBhatta <96721563+PankajBhatta@users.noreply.github.com>
2024-01-07 14:38:26 +0545

    search

--- frontend/src/app/components/partials/search/search.component.ts
+++ frontend/src/app/components/partials/search/search.component.ts
@@ -1,25 +1,24 @@
-import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-// import { Router } from 'express';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+export class SearchComponent implements OnInit {
+  searchTerm = '';
+  constructor(activatedRoute:ActivatedRoute,private router:Router) {
+    activatedRoute.params.subscribe((params) => {
+      if(params.searchTerm) this.searchTerm = params.searchTerm;
+    });
+   }
 
-export class SearchComponent implements OnInit{
-  SearchTerm='';
-  router: any;
-  constructor(private route: ActivatedRoute) {
-    this.route.params.subscribe(params => {
-      const query = params['query'];
-});
-  }
   ngOnInit(): void {
   }
-  Search(term:string):void{
+
+  search(term:string):void{
     if(term)
-    this.router.navigateByUrl('/search'+ term);
+    this.router.navigateByUrl('/search/'+ term);
   }
 }
